0554e100c41017a67b0416a4a92f806962b998ce
tojoirinah <tojoirinah@gmail.com>
2019-12-20 13:12:32 +0100

    add function evaluation + alter generate comment

---
+++ Appreciation.Manager.Database/Scripts/20191220-alter-dbo.sp_GenerateComment.sql
@@ -0,0 +1,71 @@
+﻿ALTER PROCEDURE [dbo].[sp_GenerateComment]
+AS  
+BEGIN  
+    SET NOCOUNT OFF;  
+  
+   
+
+	BEGIN TRANSACTION transStudentExam
+    BEGIN TRY
+		DECLARE @last_school_year_id BIGINT = 0
+		SELECT TOP 1 @last_school_year_id = [Id] FROM [dbo].[SchoolYear] WHERE [IsClosed] = 0 ORDER BY [Id] DESC
+
+		CREATE TABLE #tStudentExam
+		(
+			Id BIGINT NOT NULL,
+			NoteEvaluate VARCHAR(MAX) NULL,
+			BehaviorEvaluate VARCHAR(MAX) NULL,
+			DateCreated DATETIME NOT NULL
+		)
+        
+		print convert(varchar(5),@last_school_year_id)
+		IF @last_school_year_id <> 0
+		BEGIN
+			INSERT INTO #tStudentExam(Id,NoteEvaluate,BehaviorEvaluate,DateCreated)
+			(
+				SELECT 
+					se.Id,
+					dbo.fnGetNoteEvaluate(st.Id,ne.Evaluation + ' .'+ ne.Advice),
+					dbo.fnGetNoteEvaluate(st.Id,be.Evaluation),
+					GETDATE()
+				FROM [dbo].[Student] st
+				INNER JOIN [dbo].[StudentExam] se ON se.[StudentId] = st.[Id]
+				INNER JOIN [dbo].[NoteCriteria] nc ON (nc.[Min] <= se.Note AND se.Note <= nc.Max)
+				INNER JOIN [dbo].[NoteEvaluate] ne ON ne.NoteCriteriaId = nc.Id
+				INNER JOIN [dbo].[BehaviorEvaluate] be ON (be.[BehaviorId] = se.[BehaviorId] AND be.NoteCriteriaId = ne.NoteCriteriaId)
+				WHERE st.[SchoolYearId] = @last_school_year_id
+				AND se.[IsClosed] = 0
+			)
+
+			IF EXISTS(SELECT 1 FROM #tStudentExam)
+			BEGIN
+				UPDATE se
+				SET se.NoteEvaluate = tmp.NoteEvaluate,
+				se.BehaviorEvaluate = tmp.BehaviorEvaluate,
+				se.DateCreated = tmp.DateCreated
+				FROM [dbo].[StudentExam] se 
+				INNER JOIN #tStudentExam tmp ON tmp.[Id] = se.[Id]
+				select * from #tStudentExam
+
+			END
+		END
+
+		COMMIT TRANSACTION transStudentExam
+	END TRY
+	BEGIN CATCH 
+        SELECT
+            ERROR_NUMBER() AS ErrorNumber,
+            ERROR_SEVERITY() AS ErrorSeverity,
+            ERROR_STATE() AS ErrorState,
+            ERROR_PROCEDURE() AS ErrorProcedure,
+            ERROR_LINE() AS ErrorLine,
+            ERROR_MESSAGE() AS ErrorMessage
+
+            ROLLBACK TRANSACTION transStudentExam
+
+    END CATCH
+
+END
+GO
+
+
---
+++ Appreciation.Manager.Database/Scripts/20191220-dbo.fnGetNoteEvaluater.sql
@@ -0,0 +1,24 @@
+﻿if(object_id(N'[dbo].[fnGetNoteEvaluate]', N'FN')) is not null
+    drop function [dbo].[fnGetNoteEvaluate];
+GO
+
+CREATE FUNCTION dbo.fnGetNoteEvaluate (@studentId BIGINT,@noteEvaluate VARCHAR(MAX))
+RETURNS VARCHAR(MAX)
+AS BEGIN
+    DECLARE @Work VARCHAR(MAX) = @noteEvaluate
+	DECLARE @userFirstName VARCHAR(100) = '', @pronom VARCHAR(5) = ''
+	
+
+	SELECT @userFirstName = FirstName,
+		   @pronom = (CASE WHEN s.Civility = 1 THEN 'Il' ELSE 'Elle' END)
+		FROM [dbo].[User] u 
+		INNER JOIN [dbo].[Student] s ON s.[UserId] = u.[Id]
+		WHERE s.[Id] = @studentId
+
+	IF @userFirstName <> ''
+	BEGIN
+		SET @Work = REPLACE(@Work, '_name_', @userFirstName)
+		SET @Work = REPLACE(@Work, '_pronom_', @pronom)
+	END
+    RETURN @work
+END
