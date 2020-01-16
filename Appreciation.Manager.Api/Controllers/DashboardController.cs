8a5ab5f587ade65557a627ea9968dba63f5579a9
tojoirinah <tojoirinah@gmail.com>
2020-01-16 17:15:26 +0100

    gestion version

--- Appreciation.Manager.Api/Controllers/DashboardController.cs
+++ Appreciation.Manager.Api/Controllers/DashboardController.cs
@@ -39,7 +39,7 @@ namespace Appreciation.Manager.Api.Controllers
         {
             try
             {
-                // test v1.0
+                // test v2.0
                 await _service.GenerateStudentExam();
                 await _service.CommitAsync();
                 return Ok();
